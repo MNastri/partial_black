3b7f4aa7394fc5f56cf7d6efd9c5eca138e8936b
MNastri <marcellonastri@gmail.com>
2023-02-17 18:51:11 -0300

    style/improve reading

--- partial_black.py
+++ partial_black.py
@@ -1,10 +1,9 @@
 """
-It is partial black in python to be utilized in windows PyCharm.
-- To see partial black here (https://blog.godatadriven.com/black-formatting-selection)
-- This code is conversion of this shell script (https://gist.github.com/BasPH/5e665273d5e4cb8a8eefb6f9d43b0b6d)
-
-If you want to use in windows, you should first complie this python to exe.
-- To do that, see (https://pypi.org/project/auto-py-to-exe/)
+Script to be used with Pycharm
+Partial black (https://blog.godatadriven.com/black-formatting-selection)
+Original shell script (https://gist.github.com/BasPH/5e665273d5e4cb8a8eefb6f9d43b0b6d)
+Original GitHub Gist (https://gist.github.com/imcomking/04d8ce30b6ac444e413392ab675b1d2c)
+On Windows you have to compile to exe (https://pypi.org/project/auto-py-to-exe/)
 """
 import os
 import re
@@ -19,7 +18,6 @@ input_file = sys.argv[2]
 start_line = int(sys.argv[3]) - 1
 end_line = int(sys.argv[4])
 
-
 # read input_file
 with open(input_file, "rt", encoding="utf-8") as src_file:
     src_contents = [line for line in src_file]
@@ -33,22 +31,18 @@ selection = [l[indent_spaces:] for l in src_contents[start_line:end_line]]
 # print(updated_lines)
 print("Total file len: ", len(src_contents), ". selected lines: ", len(selection))
 
-
-# it is a workaround for escaping windows os permission problem.
+# escaping Windows permission problem
 tmp_dir = tempfile.TemporaryDirectory()
 tmp_file_name = os.path.join(tmp_dir.name, "tmp_file_for_black")
-
 
 # write selection on tmp_file
 with open(tmp_file_name, "wt", encoding="utf-8") as f:
     f.writelines(selection)
 
-
 # run black on tmp_file
 cmd = black + " " + tmp_file_name
 print("Run cmd:", cmd)
 os.system(cmd)
-
 
 # apply reformatted selection to origianl source file
 with open(tmp_file_name, "rt", encoding="utf-8") as f:
@@ -57,7 +51,6 @@ with open(tmp_file_name, "rt", encoding="utf-8") as f:
         # include indentation
         ident_line = " " * indent_spaces + line
         src_contents.insert(start_line + i, line)
-
 
 # overwrite it to input_file
 with open(input_file, "wt", encoding="utf-8") as f:
